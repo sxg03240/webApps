b204c532f2d86295792f7f04c2f0beef53dabba3
Matha Rakesh <r0m0iek@homeoffice.wal-mart.com>
2023-02-21 11:39:51 -0600

    Changes for resolution match

--- Assignment1/attendance.js
+++ Assignment1/attendance.js
@@ -1,30 +1,31 @@
-"use strict;"
+"use strict;";
 
-let classStudents = 0
-slideShow()
+const rows =
+  ".firstRow , .secondRow , .thirdRow, .fourthRow, .fifthRow, .sixthRow, .seventhRow, .eightRow";
+let classStudents = 0;
+slideShow();
 
 function slideShow() {
-    var images = document.getElementsByClassName("students")
-    for (let i = 0; i < images.length; i++) {
-        images[i].style.display = "none"
-    }
-    classStudents++
-    if (classStudents > images.length) {
-        classStudents = 1
-    }
-    images[classStudents - 1].style.display = "block"
-    setTimeout(slideShow, 1000)
+  var images = document.getElementsByClassName("students");
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  classStudents++;
+  if (classStudents > images.length) {
+    classStudents = 1;
+  }
+  images[classStudents - 1].style.display = "block";
+  setTimeout(slideShow, 1000);
 }
 
 $(document).ready(function () {
-    $("td").click(function () {
-        $(this).toggleClass("checked")
-        var absentees = []
-        $("td").each(function () {
-            if ($(this).hasClass("checked"))
-                absentees++
-        });
-        var studentsAbsent = document.getElementById("noOfAbsentees")
-        studentsAbsent.innerHTML = absentees
-    })
-})
+  $(rows).click(function () {
+    $(this).toggleClass("checked");
+    var absentees = [];
+    $(rows).each(function () {
+      if ($(this).hasClass("checked")) absentees++;
+    });
+    var studentsAbsent = document.getElementById("noOfAbsentees");
+    studentsAbsent.innerHTML = absentees;
+  });
+});
--- Homework3/Ex2/faqs_collapse.js
+++ Homework3/Ex2/faqs_collapse.js
@@ -4,29 +4,29 @@
     var h2Elements = faqs.getElementsByTagName("h2");
     var h2Length = h2Elements.length
     window.onload = function() {
-        
+
     function displayFaq(h2){
             var div = h2.nextElementSibling;
             h2.removeAttribute("class")
             div.setAttribute("class", "open")
         }
-      
+
     function collapseFaq(h2){
             var div = h2.nextElementSibling;
             h2.setAttribute("class", "minus")
             div.removeAttribute("class")
         }
-      
+
     function displayOnClick(){
            for(var i=0; i < h2Length; i++){
-            var h2 = h2Elements[i]; 
-             if(h2 == this){  
-               if(!h2.hasAttribute("class")){  
+            var h2 = h2Elements[i];
+             if(h2 == this){
+               if(!h2.hasAttribute("class")){
                 collapseFaq(h2);
-               } else{  
-                displayFaq(h2); 
+               } else{
+                displayFaq(h2);
                  }
-             } else{  
+             } else{
               collapseFaq(h2);
              }
            }
@@ -35,5 +35,5 @@
     for (var i = 0; i < h2Length; i++ ) {
             h2Elements[i].onclick = displayOnClick
             collapseFaq(h2Elements[i])
-        }     
+        }
     }

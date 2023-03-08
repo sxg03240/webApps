0841a75a4323149e84f6923b581ac932bc65660a
sxg03240 <112096651+sxg03240@users.noreply.github.com>
2023-03-08 00:10:15 -0600

    Modifications to animate cards

--- Assignment2/poker.js
+++ Assignment2/poker.js
@@ -1,106 +1,116 @@
 window.onload = () => {
-  let shuffle = [];
-  const deal = document.getElementById('deal');
-  const result = document.getElementById('result');
-  const bet = document.getElementById('btn');
-  shuffleShowCheck();
-  //shuffle and show cards when load
+  let shuffle = []
+  const deal = document.getElementById('deal')
+  const result = document.getElementById('result')
+  const bet = document.getElementById('btn')
+  const nextGame = document.getElementsByTagName('button')[0]
+  shuffleShowCheck()
   function shuffleShowCheck() {
-    shuffle.length = 0;
-    deal.innerHTML = "";
-    const usedCards = new Set();
+    shuffle.length = 0
+    deal.innerHTML = ""
+    const usedCards = new Set()
     while (shuffle.length < 5) {
       const card = {
         suit: String.fromCharCode(Math.floor(Math.random() * 4) + 97),
         num: Math.floor((Math.random() * 13) + 1)
-      };
-      // Check if the card is a duplicate
+      }
       if (usedCards.has(`${card.num}-${card.suit}`)) {
-        console.log('Duplicate found. Starting over...');
-        continue;
+        console.log('Duplicate found. Starting over...')
+        continue
       }
-      // Add the card to the shuffle and the set of used cards
-      shuffle.push(card);
-      usedCards.add(`${card.num}-${card.suit}`);
+      shuffle.push(card)
+      usedCards.add(`${card.num}-${card.suit}`)
     }
-    showCards();
+    showCards()
   }
-  //show cards
+
+
   function showCards() {
-    const cardTemplate = document.getElementsByTagName('template')[0];
-    const cardItem = cardTemplate.content.querySelector('.card');
-    let a;
+    const cardTemplate = document.getElementsByTagName('template')[0]
+    const cardItem = cardTemplate.content.querySelector('.card')
+    let a
     for (let i = 0; i < shuffle.length; i++) {
-      a = document.importNode(cardItem, true);
-      a.style.setProperty('--i', i); // set --i variable
+      a = document.importNode(cardItem, true)
       if (i < 2) {
-        // Show the first two cards
         Object.assign(a.style, {
-          backgroundImage: `url('images/cards/${shuffle[i].num}.png')`,
+          backgroundImage: "url('images/cards/" + shuffle[i].suit + "/" + shuffle[i].num + ".png')",
           visibility: 'visible',
-          left: `${i * 150}px`,
-          animationDelay: `${0.5 * i}s`
-        });
+        })
       } else {
-        // Hide the remaining three cards
         Object.assign(a.style, {
-          backgroundImage: "url('images/rearSideCard.jpg')",
+          backgroundImage: "url('images/card_back.jpg')",
           visibility: "visible",
-          left: `${i * 150 - 300}px`,
-          animationDelay: `${0.9 * i}s`
-        });
+        })
       }
-      deal.appendChild(a);
+      deal.appendChild(a)
     }
   }
-  //BET Button
+
+
   bet.onclick = () => {
     bet.value = "Bet"
     bet.innerHTML = "Next Game"
-    // Get the bet amount from the input field
-    const betAmount = parseInt(document.getElementById("bet-amount").value);
-    // Validate the bet amount
-    if (isNaN(betAmount) || betAmount < 1 || betAmount > 10) {
-      alert("Please enter a valid bet amount between 1 and 10.");
-      return;
+    bet.classList.add("nextGame")
+    const remainingCards = shuffle.slice(2)
+    for (let i = 0; i < remainingCards.length; i++) {
+      const cardElement = deal.childNodes[i + 2]
+      setTimeout(() => {
+        cardElement.classList.add("flipping")
+        setTimeout(() => {
+          cardElement.style.backgroundImage = "url('images/cards/" + remainingCards[i].suit + "/" + remainingCards[i].num + ".png')"
+        }, i * 20)
+      }, i * 500)
     }
-    // Show the remaining three cards
-    for (let i = 2; i < shuffle.length; i++) {
-      const cardElement = deal.childNodes[i];
-      cardElement.style.backgroundImage = `url('images/cards/${shuffle[i].num}.png')`;
-    }
-    updateScore();
-    // Update the player's balance and clear the bet input field
-    balance -= betAmount;
-    document.getElementById("balance").textContent = balance;
-    document.getElementById("bet-amount").value = "";
+    updateScore()
   }
-  //Reset
-  const resetButton = document.getElementById("reset");
+
+
+  // nextGame.onclick = () => {
+  //   bet.value = "Next Game"
+  //   bet.innerHTML = "Bet"
+  //   const remainingCards = shuffle.slice(5)
+  //   shuffle = shuffle.slice(0, 5).concat(shuffleCards(remainingCards))
+  //   for (let i = 0; i < 5; i++) {
+  //     const cardElement = deal.childNodes[i + 2]
+  //     cardElement.style.backgroundImage = "url('images/cards/" + shuffle[i].suit + "/" + shuffle[i].num + ".png')"
+  //   }
+  //   bet.value = "Bet"
+  //   bet.innerHTML = "Bet"
+  //   updateScore()
+  // }
+
+
+
+  const resetButton = document.getElementById("reset")
   resetButton.addEventListener("click", function () {
-    location.reload();
-  });
-  //update score
+    location.reload()
+  })
   function updateScore() {
-    let score = 0;
-    // Calculate the score for the current hand
+    let score = 0
     for (let i = 0; i < shuffle.length; i++) {
-      const card = shuffle[i];
+      const card = shuffle[i]
       if (card.num === 1) {
-        score += 11; // Ace
+        score += 11
       } else if (card.num >= 10) {
-        score += 10; // Face card or 10
+        score += 10
       } else {
-        score += card.num; // Number card
+        score += card.num
       }
     }
-    // Adjust the score if there is an Ace
     for (let i = 0; i < shuffle.length; i++) {
       if (shuffle[i].num === 1 && score > 21) {
-        score -= 10;
+        score -= 10
       }
     }
-    // Update the score in the heading
-    document.getElementById("score").textContent = "Score: " + score;
+    document.getElementById("score").textContent = "Score: " + score
+  }
+  function highlightPointsRow(index) {
+    const rows = document.querySelectorAll(".points .hand");
+    for (let i = 0; i < rows.length; i += 2) {
+      rows[i].classList.remove("highlight");
+    }
+    if (index >= 0) {
+      rows[index].classList.add("highlight");
+    }
   }
 }
